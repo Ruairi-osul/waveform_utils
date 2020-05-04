4d1df5e1fbb447c72da4f865328403d7689fffcf
Ruairi-osul <rorylikesmusic@gmail.com>
2020-05-04 16:21:54 +0100

    changed website

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    reqs = f.read().splitlines()
+
+description = ""
+long_description = ""
+setup(
+    name="waveform_utils",
+    description=description,
+    long_description=long_description,
+    version="0.0.1a1",
+    url="https://github.com/Ruairi-osul/waveform_utils",
+    author="Ruairi O'Sullivan",
+    author_email="ruairi.osullivan.work@gmail.ie",
+    license="GNU GPLv3",
+    keywords="neuroscience science electrophysiology ephys",
+    project_urls={"Source": "https://github.com/Ruairi-osul/waveform_utils"},
+    packages=find_packages(),
+    python_requires=">=3.3",
+    install_requires=reqs,
+)
